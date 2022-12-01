1c5c831e8246a6421ce5e2bd572ff291fc953a88
TolibjonD <stolibjon123@gmail.com>
2022-12-01 23:50:15 +0500

    Update 13-dars.py

--- 13-dars.py
+++ 13-dars.py
@@ -1,5 +1,6 @@
-dividend = int(input("Please Enter the number you want >> "))
+# any number dividing without rest
 
+dividend = int(input("Please Enter the number you want >> "))
 for divisor in range(2,11):
     if not (dividend % divisor):
         print(f"{divisor} is divisible by {dividend} without a remainder ")
