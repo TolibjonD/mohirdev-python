b23654a7ce71eef906db5575fcd1a9685304ab77
TolibjonD <stolibjon123@gmail.com>
2022-12-07 18:51:55 +0500

    Updates main.py
    I have added some changes in my project

--- 29-dars/main.py
+++ 29-dars/main.py
@@ -8,7 +8,7 @@ class Avto:
         self.mileage = 0
 
     def getInfo(self):
-        info = f"{self.model} has color {self.color} ."
+        info = f"The color of {self.model} is {self.color} ."
         info += f"Transmission is {self.transmission} and price: $ {self.price} /- ."
         info +=f" Mileage: {self.mileage} km "
         return info
@@ -16,10 +16,24 @@ class Avto:
     def updateMileage(self, distance):
         self.mileage += distance
 
-    def converterMoney(self):
+    def Currency(self):
         result = f"Uzb so'm:  {self.price * 11333,55} so'm"
         return result
 # Avto klassdan ob'ekt yaratib olamiz
 Nexia = Avto("Nexia" , "White Summit" , 10000 , "Automatic")
 
-        
+# Avtosalon nomli class
+class Avtosalon:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.cars = [Nexia]
+    
+    def addCar(self, car):
+        self.cars.append(car)
+        msg = f"{car.model} have added successfully into cars !..."
+        return msg
+    
+    def showCars(self):
+        return [car.getInfo() for car in self.cars]
+pokiza = Avtosalon("Pokiza" , "Pop tumani")
